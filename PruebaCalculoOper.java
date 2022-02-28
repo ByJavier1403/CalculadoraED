623142fc7a7245b1f2b39eda88320945ef3b087e
TaniaDomp <98829975+TaniaDomp@users.noreply.github.com>
2022-02-28 07:02:36 -0600

    Update PruebaCalculoOper.java

--- PruebaCalculoOper.java
+++ PruebaCalculoOper.java
@@ -1,22 +1,31 @@
 package pila_01;
-/**
- *
- * @author tania
+/**@authorTania Ariadna Dominguez Palma
+ *27/02/2022
+ *Clase que prueba la clase CalculoOperaciones
  */
 public class PruebaCalculoOper {
     
     public static void main(String[] args) {
         PilaA<String> cad = new PilaA();
+        PilaA<String> cad1 = new PilaA();
+        PilaA<String> cad2 = new PilaA();
         PilaA num = new PilaA();
         
-        cad.push("5");
-        cad.push("10");
-        cad.push("15");
-        cad.push("-48.25");
-        //cad.push("-");
-        //cad.push("^");
-        
+        cad.push("3");
+        cad.push("4");
+        cad.push("6");
+        //cad.push("-48.25");
+        cad.push("*");
+        cad.push("/");
+        cad1.push("-25");
+        cad1.push("-2.3");
+        cad1.push("*");
+        cad2.push("5");
+        cad2.push("4");
+        cad2.push("-");
         System.out.println(CalculoOperaciones.calculo(cad));
+        System.out.println(CalculoOperaciones.calculo(cad1));
+        System.out.println(CalculoOperaciones.calculo(cad2));
         //System.out.println(CalculoOperaciones.calculo(cad).toString());
     }
 }
